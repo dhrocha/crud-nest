2f55b4898964c1af1c7e9d0835ca5b139043b0d3
Daniel Rocha <rochadhc@gmail.com>
2020-08-31 16:15:41 -0300

    initial commit

---
+++ src/database/mysql.provider.ts
@@ -0,0 +1,14 @@
+import { Injectable, Logger } from '@nestjs/common'
+
+@Injectable()
+export class MySQLProvider {
+  private readonly logger: Logger
+  constructor() {
+    this.logger = new Logger('MySQLProvider')
+    this.logger.log('Initialized')
+  }
+
+  getValue(): string {
+    return 'mysql ok'
+  }
+}
---
+++ src/modules/product/product.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common'
+import { MySQLProvider } from 'src/database/mysql.provider'
+
+@Injectable()
+export class ProductService {
+  constructor(private readonly mysql: MySQLProvider) {}
+  async findAll(): Promise<any> {
+    console.log(this.mysql.getValue())
+  }
+}
