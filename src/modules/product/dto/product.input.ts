511d0947f8b72147c6c86e58b7a7d0e5a9e58963
Daniel Rocha <rochadhc@gmail.com>
2020-09-01 00:31:41 -0300

    resolvers graphql

---
+++ src/modules/product/dto/product.input.ts
@@ -0,0 +1,13 @@
+import { Field, InputType } from '@nestjs/graphql'
+
+@InputType()
+export class ProductInput {
+  @Field()
+  id: number
+
+  @Field()
+  product: string
+
+  @Field()
+  price: number
+}
